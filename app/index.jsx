057b91cac7e15d034aa0bdf31093daa058efe740
Karim <kinottohw@gmail.com>
2017-11-07 18:39:21 +0100

    first commit front

---
+++ app/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Test from './components/test';
+// console.log(Test);
+
+ReactDOM.render(<Test/>, document.querySelector('.output'));
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const PORT = process.env.PORT || 3003;
+module.exports = {
+    entry: './app/index.jsx',
+    output: {
+      publicPath: "/",
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js'
+    },
+    devtool: 'eval-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          enforce: "pre",
+          exclude: /node_modules/,
+          loader: "eslint-loader",
+          options: {
+              fix: true,
+          },
+        },
+        {
+          test: /\.(js|jsx)$/,
+          use: {
+              loader: 'babel-loader',
+              options: {
+                  presets: ['react', 'env', 'flow']
+              }
+          },
+          exclude: /node_modules/
+        },
+
+        {
+          test: /\.scss$/,
+          use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
+              use: [{
+                      loader: "css-loader" // translates CSS into CommonJS
+                  },
+                  {
+                      loader: "sass-loader" // compiles Sass to CSS
+                  }
+              ],
+              fallback: "style-loader" // used when css not extracted
+          }))
+        }
+      ]
+    },
+    devServer: {
+      hot: true,
+      port: PORT
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new ExtractTextPlugin({
+            filename: 'styles.css',
+            allChunks: true
+        })
+    ]
+}
